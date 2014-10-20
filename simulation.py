2d27080288fd4d846817c2b6210315fcbd16199c
unknown <awood314@gmail.com>
2014-10-20 13:41:55 -0700

    simple sizer + panel + image

--- simulation.py
+++ simulation.py
@@ -12,7 +12,24 @@ class SimulationApp(wx.Frame):
     self.initialize()
 
   def initialize(self):
-    self.Show(True)
+    # Grid layout
+    self.sizer = wx.BoxSizer(wx.VERTICAL)
+
+    # Background image
+    self.bg = wx.Image('bg.gif', wx.BITMAP_TYPE_GIF)
+    self.panel = wx.Panel(self, pos=wx.Point(100,100), size=wx.Size(400, 400))
+    self.bg_bmp = wx.StaticBitmap(self.panel, 1, wx.BitmapFromImage(self.bg))
+    self.sizer.Add(self.panel)
+
+    # Speed Slider
+    self.tempo = wx.Slider(self)
+    self.sizer.Add(self.tempo, flag=wx.ALIGN_CENTER)
+
+    # Set Layout
+    self.SetSizer(self.sizer)
+    self.SetAutoLayout(1)
+    self.sizer.Fit(self)
+    self.Show()
 
 if __name__ == "__main__":
   app = wx.App()
