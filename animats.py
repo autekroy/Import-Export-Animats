8ed3abfd1230c2b9a6562cbb9d58c286ffc5a79a
Alex <awood314@gmail.com>
2014-10-23 18:31:53 -0700

    random movement rather than velocities

--- animats.py
+++ animats.py
@@ -9,7 +9,7 @@ class Environment:
     self.num_animats = num_animats
     self.animats = []
     for i in range(0, num_animats):
-      self.animats.append(Animat(0, 0, random.random(), random.random()))
+      self.animats.append(Animat(random.random()*width, random.random()*height))
 
   # TODO - Get this on a thread
   def update(self):
@@ -18,37 +18,40 @@ class Environment:
       # check death
       if animat.fruit_hunger < 0 or animat.veggie_hunger < 0:
         self.animats.remove(animat)
-
       # check ceiling/floor collision
-      if animat.y < 0 or (animat.y + 30) > self.height:
-        animat.vy = animat.vy * -1
+      if animat.y < 0:
+        animat.y = 0
+      if (animat.y + 30) > self.height:
+        animat.y = self.height - 30
       # wrap-around left and right border
       if (animat.x + 30) < 0:
         animat.x = self.width
       if animat.x > self.width:
         animat.x = 0
 
+moves = {
+  0: [-1, 0], # left
+  1: [1, 0],  # right
+  2: [0, -1], # up
+  3: [0, 1],  # down
+  4: [0, 0]   # no move
+}
 
 class Animat:
-  def __init__(self, x, y, vx, vy):
-    # kinetics
+  def __init__(self, x, y):
+    # position
     self.x = x
     self.y = y
-    self.vx = (random.random() - .5) # range [-.5, .5]
-    self.vy = (random.random() - .5) # range [-.5, .5]
-
     # hunger
     self.fruit_hunger = 10
-    self.veggie_hunger = 10
-
-  def set_vx(self, vx):
-    self.vx = vx
-  def set_vy(self, vy):
-    self.vy = vy
+    self.veggie_hunger = 10 
 
   def update(self): 
-    self.x += self.vx
-    self.y += self.vy
+    # movement
+    decision = int(random.random()*4)
+    self.x += moves[decision][0]
+    self.y += moves[decision][1]
     # get hungry
-    self.fruit_hunger -= .01*math.sqrt(self.vx*self.vx + self.vy*self.vy)
-    self.veggie_hunger -= .01*math.sqrt(self.vx*self.vx + self.vy*self.vy)
+    if decision != 4:
+      self.fruit_hunger -= .001
+      self.veggie_hunger -= .001
