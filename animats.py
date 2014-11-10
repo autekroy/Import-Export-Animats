e9db74432c120042f86258840c45da478b22f392
Alex <awood314@gmail.com>
2014-11-10 01:30:58 -0800

    rudimentary collision...

--- animats.py
+++ animats.py
@@ -18,6 +18,17 @@ class Environment:
     for animat in self.animats:
       animat.update()
       
+      # check animat collision on forward moves
+      new_x = animat.x + math.cos(animat.direction*math.pi / 180)
+      new_y = animat.y + math.sin(animat.direction*math.pi / 180)
+      for other in self.animats:
+	if pow(new_x - other.x, 2) + pow(new_y - other.y, 2) <= Animat.radius:
+	  animat.wants_to_move = False
+	  break
+      if animat.wants_to_move:
+	animat.x = new_x
+	animat.y = new_y
+
       # # check death and then reproduce one animat from one of existing animats
       # if animat.fruit_hunger < 0 or animat.veggie_hunger < 0:  
       #   self.animats.remove(animat)
@@ -35,6 +46,8 @@ class Environment:
         animat.x = 0
 
 class Animat:
+  radius = 1
+
   def __init__(self, x, y, direction):
     # position
     self.x = x
@@ -50,16 +63,18 @@ class Animat:
     decision = int(random.random()*30)
 
     # forward move in 28/30 possibility
-    if decision >= 0 or dicision < 28:
-      self.x += math.cos(self.direction*math.pi / 180) * 3
-      self.y += math.sin(self.direction*math.pi / 180) * 3
+    # can't move until collision is detected
+    if decision >= 0 and decision < 28:
+      self.wants_to_move = True
+    else:
+      self.wants_to_move = False
+
     # rotate left in 1/30 possibility
     if decision == 28:
       self.direction -= 20
     # rotate right in 1/30 possibility
     if decision == 29:
       self.direction += 20
-
 
     # get hungry
     energyConsume = 0.5 # energy comsumption unit
